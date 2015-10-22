2240ef9a6efb011ff607765ffacd81e77aa3895c
Andreas Arnol <andreasarnold02@gmail.com>
2015-10-22 15:38:34 +0700

    Add breadcrumb and styling edit profile page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,24 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include Layoutable
 
+  def edit
+  	breadcrumb
+  	
+  	super
+  end
+
+  def update
+		breadcrumb
+
+  	super
+  end
+
   private
+  	def breadcrumb
+	  	add_breadcrumb "Dashboard", :dashboard_url
+	  	add_breadcrumb "Edit Profile", :edit_user_registration_url
+  	end
+
     def after_inactive_sign_up_path_for(resource)
       new_user_session_path
     end
