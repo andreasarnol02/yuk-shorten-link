cf82f6a285b68d206cec3de814678a6e1bcea510
Andreas Arnol <andreasarnold02@gmail.com>
2015-10-22 11:32:32 +0700

    Todays statistic on dashboard home page

--- app/controllers/dashboard/home_controller.rb
+++ app/controllers/dashboard/home_controller.rb
@@ -6,6 +6,14 @@ class Dashboard::HomeController < ApplicationController
   add_breadcrumb "Dashboard", :dashboard_url
 
   def index
-  	add_breadcrumb "Home"
+    add_breadcrumb "Home"
+
+    url_ids        = current_user.urls.map{ |e| e.id.to_s }
+    ahoy_track     = Ahoy::Event.track_url_self(url_ids, false).where(time: Date.today..Time.now)
+    today_clicks   = Url.where(id: ahoy_track.map { |e| e.properties["url_id"] })
+
+    @today_clicks_count = ahoy_track.count
+    @top_10_today       = today_clicks.order(click_count: :desc).limit(10)
+    @today_clicks       = today_clicks.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
